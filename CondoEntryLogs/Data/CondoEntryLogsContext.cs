929d1e49094feb9efefc6115bfc10695e9db546b
nicoleyaghoobi <53989251+nicoleyaghoobi@users.noreply.github.com>
2019-10-19 14:40:16 -0400

    condo entry css works BAD formatting
    condo entry css works BAD formatting

---
+++ CondoEntryLogs/Data/CondoEntryLogsContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace CondoEntryLogs.Models
+{
+    public class CondoEntryLogsContext : DbContext
+    {
+        internal IEnumerable<object> Visitors;
+
+        public CondoEntryLogsContext (DbContextOptions<CondoEntryLogsContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<CondoEntryLogs.Models.Visitor> Visitor { get; set; }
+    }
+}
---
+++ CondoEntryLogs/Models/Visitor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CondoEntryLogs.Models
+{
+    public class Visitor
+    {
+        [Key]
+        public int Id { get; set; }
+        [DisplayName("First name")]
+        [Required]
+        public string FirstName { get; set; }
+        [DisplayName("Last name")]
+        [Required]
+        public string LastName { get; set; }
+        [DisplayName("Phone number")]
+        [Required]
+        public string PhoneNumber { get; set; }
+        [DisplayName("Time of entry")]
+        public DateTime? TimeOfEntry { get; set; }
+        [DisplayName("Time of exit")]
+        public DateTime? TimeOfExit { get; set; }
+        [DisplayName("Unit number")]
+        [Required]
+        public int UnitNumber { get; set; }
+    }
+}
